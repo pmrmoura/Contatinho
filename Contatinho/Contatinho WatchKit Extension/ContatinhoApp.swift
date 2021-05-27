f3d2c7bb03f4d98e47ef2211e21884166fc9a66e
Pedro da Matta <pmrm@cin.ufpe.br>
2021-05-27 09:39:16 -0300

    Setup(project): add initial xcode project

---
+++ Contatinho/Contatinho WatchKit Extension/ContatinhoApp.swift
@@ -0,0 +1,21 @@
+//
+//  ContatinhoApp.swift
+//  Contatinho WatchKit Extension
+//
+//  Created by Pedro Moura on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ContatinhoApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
---
+++ Contatinho/Contatinho WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Contatinho WatchKit Extension
+//
+//  Created by Pedro Moura on 27/05/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
