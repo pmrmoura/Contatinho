4e6dbcfae36a6c7012ee2d29165e2ee12b6e9f48
Pedro da Matta <pmrm@cin.ufpe.br>
2021-06-01 11:05:02 -0300

    Fix(QRCode): Loading)

--- Contatinho/Contatinho WatchKit Extension/View/QRCodeView.swift
+++ Contatinho/Contatinho WatchKit Extension/View/QRCodeView.swift
@@ -11,15 +11,15 @@ import Contacts
 struct QRCodeView: View {
     @Environment(\.presentationMode) var presentationMode
     @Binding var backFromQRCodeView: Bool
-//    @StateObject var viewModel: QRCodeViewModel = QRCodeViewModel()
+    @StateObject var viewModel: QRCodeViewModel = QRCodeViewModel()
     let contact: Contact
     var body: some View {
         VStack {
             
-//            Image(viewModel.generateQRCode(contact: contact)!, scale: 2.0, orientation: .up, label: Text("oi"))
-//                .resizable()
-//                .scaledToFit()
-//                .padding()
+            Image(viewModel.generateQRCode(contact: contact)!, scale: 2.0, orientation: .up, label: Text("oi"))
+                .resizable()
+                .scaledToFit()
+                .padding()
 
             Button(action: {
                 self.backFromQRCodeView = true
